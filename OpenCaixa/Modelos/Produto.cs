f66ee75c96e2318e86b9574a41541e30a71e5a94
Lucas Leandro <lucasjleandro@gmail.com>
2021-11-06 22:06:48 -0300

    Add project files.

---
+++ OpenCaixa/Modelos/Produto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCaixa.Modelos
+{
+    public class Produto
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Estoque { get; set; }
+        public Categoria Categoria { get; set; }
+
+
+        public Produto(string nome, double preco, int estoque = 0, Categoria categoria)
+        {
+            Nome = nome;
+            Preco = preco;
+            Estoque = estoque;
+            Categoria = categoria;
+        }
+    }
+}
