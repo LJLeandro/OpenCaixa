d74670fc3a87acab68502420e28fcf048f12bb31
Lucas Leandro <lucasjleandro@gmail.com>
2021-11-07 14:01:00 -0300

    Modelo concluído

--- OpenCaixa/Modelos/Movimentacao.cs
+++ OpenCaixa/Modelos/Movimentacao.cs
@@ -4,6 +4,14 @@
     {
         public int Codigo { get; set; }
         public int Quantidade { get; set; }
-        public int MyProperty { get; set; }
+        public double Valor { get; set; }
+        public TipoMovimentacao TipoMovimentacao { get; set; }
+        public Produto? Produto { get; set; }
+        public ItemVenda? ItemVenda { get; set; }
+    }
+
+    public enum TipoMovimentacao
+    {
+        Entrada, Saida, Fechamento_Caixa
     }
 }
---
+++ OpenCaixa/Modelos/Usuario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCaixa.Modelos
+{
+    class Usuario
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Apelido { get; set; }
+        public Permissao Permissao { get; set; }
+        public string Senha { get; set; }
+
+        public Usuario(int codigo, string nome, string apelido, Permissao permissao, string senha)
+        {
+            Codigo = codigo;
+            Nome = nome;
+            Apelido = apelido;
+            Permissao = permissao;
+            Senha = senha;
+        }
+    }
+
+    public enum Permissao
+    {
+        Caixa, Gerente, Administrador
+    }
+}
