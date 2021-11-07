e5f587449b8d05834a08f6f4a9de5606fc86e9b3
Lucas Leandro <lucasjleandro@gmail.com>
2021-11-07 14:13:09 -0300

    Preparada a conexão com a Base MySQL

---
+++ OpenCaixa/BaseDeDados/OpenCaixaContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OpenCaixa.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCaixa.BaseDeDados
+{
+    class OpenCaixaContext : DbContext
+    {
+        public class ApplicationDBContext : DbContext
+        {
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer("server=127.0.0.1;Port=3307;database=OpenCaixaDatabase;uid=root;pwd=Pft!zM0DyQRE;");
+            }
+
+            public DbSet<Categoria> Categorias { get; set; }
+            public DbSet<ItemVenda> ItemVendas { get; set; }
+            public DbSet<Movimentacao> Movimentacaos { get; set; }
+            public DbSet<Produto> Produtos { get; set; }
+            public DbSet<Usuario> Usuarios { get; set; }
+        }
+    }
+}
